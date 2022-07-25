7cf184c859b593057d1025736971a4855df8dd7b
Neo <9janeo@gmail.com>
2022-07-25 07:49:39 -0500

    Install fb pixel script in functions

--- functions.php
+++ functions.php
@@ -203,10 +203,29 @@ function custom_head_script() {
 
 		gtag('config', 'UA-111255337-1');
 	</script>";
+
+	$pixel = "<!-- Meta Pixel Code -->";
+	$pixel .= "<script>
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window, document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '1430179477124549');
+	fbq('track', 'PageView');
+	</script>
+	<noscript><img height='1' width='1' style='display:none'
+	src='https://www.facebook.com/tr?id=1430179477124549&ev=PageView&noscript=1'
+	/></noscript>";
+	$pixel .= "<!-- End Meta Pixel Code -->";
 	
 	// echo $google;
 	echo $manager;
 	echo $ga4;
+	echo $pixel;
 
 }	
 add_action('wp_head', 'custom_head_script');
